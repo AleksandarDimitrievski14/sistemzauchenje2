80cf972502351d22e5a1d0c6c38b8b220102d710
AleksandarDimitrievski14 <dimitrievskisashe@gmail.com>
2022-09-05 00:28:17 +0200

    commit

--- sistemzauchenje/settings.py
+++ sistemzauchenje/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1hxw7)%*r$)u32&vhm%!#bf@a8iw+ro&t+*=o!lf^es%uty8(c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sistemgermanski.herokuapp.com/ ']
+ALLOWED_HOSTS = ['127.0.0.1', 'sistemgermanski.herokuapp.com']
 
 
 # Application definition
